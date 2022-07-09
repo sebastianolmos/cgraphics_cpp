5806f7ac1d346e758d488e76e07a52dc55d16d57
sebastianolmos <sebastianolmos@ug.uchile.cl>
2022-07-08 20:35:56 -0400

    refactor example ans solver peter panning problem

--- examples/11_SingleLightShadow/singleLightShadow.cpp
+++ examples/11_SingleLightShadow/singleLightShadow.cpp
@@ -56,6 +56,16 @@ struct DirectionalLight {
     glm::vec3 ambient;
     glm::vec3 diffuse;
     glm::vec3 specular;
+    //shadows
+    unsigned int depthMap;
+    unsigned int depthMapFBO;
+    glm::vec3 position;
+    glm::mat4 spaceMatrix;
+    glm::mat4 projection;
+    glm::mat4 view;
+    float nearPlane;
+    float farPlane;
+    float orthoDim;
 };
 
 struct SpotLight {
@@ -172,29 +182,6 @@ int main()
     Shader* currentLightTexShader = nullptr;
     Shader* currentLightClrShader = nullptr;
 
-    // configure depth map FBO
-    // -----------------------
-    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
-    unsigned int depthMapFBO;
-    glGenFramebuffers(1, &depthMapFBO);
-    // create depth texture
-    unsigned int depthMap;
-    glGenTextures(1, &depthMap);
-    glBindTexture(GL_TEXTURE_2D, depthMap);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
-    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-    // attach depth texture as FBO's depth buffer
-    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
-    glDrawBuffer(GL_NONE);
-    glReadBuffer(GL_NONE);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
     // shader configuration
     // --------------------
     depthDebugShader.use();
@@ -216,10 +203,14 @@ int main()
     pointLight->quadratic = 0.032f;
 
     DirectionalLight* dirLight = new DirectionalLight;
-    dirLight->direction = glm::vec3(1.0f, -1.0f, 0.0f);
+    dirLight->direction = glm::normalize(glm::vec3(1.0f, -1.0f, 0.5f));
     dirLight->ambient = glm::vec3(0.5f);
     dirLight->diffuse = glm::vec3(1.0f);
     dirLight->specular = glm::vec3(1.0f);
+    dirLight->nearPlane = 0.01f;
+    dirLight->farPlane = 17.5f;
+    dirLight->orthoDim = 10.0f;
+    dirLight->projection = glm::ortho(-dirLight->orthoDim, dirLight->orthoDim, -dirLight->orthoDim, dirLight->orthoDim, dirLight->nearPlane, dirLight->farPlane);
 
     SpotLight* spotLight = new SpotLight;
     spotLight->position = glm::vec3(1.0f);
@@ -238,6 +229,28 @@ int main()
     RenderBatch phongClrObjects;
     RenderBatch coloredObjects;
 
+    // configure depth map FBO for each light type
+    // -----------------------
+    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+    // For Directional Light
+    glGenFramebuffers(1, &(dirLight->depthMapFBO));
+    glGenTextures(1, &(dirLight->depthMap)); // create depth texture
+    glBindTexture(GL_TEXTURE_2D, (dirLight->depthMap));
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    // attach depth texture as FBO's depth buffer
+    glBindFramebuffer(GL_FRAMEBUFFER, (dirLight->depthMapFBO));
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, (dirLight->depthMap), 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
     // Phong textured objects
     RenderObjectPtr floor = createTexCube("assets/wood.png", 5.0f);
     floor->transform = glm::translate(floor->transform, glm::vec3(0.0f, -8.0f, 0.0f));
@@ -248,6 +261,10 @@ int main()
     box->transform = glm::rotate(box->transform, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     box->transform = glm::scale(box->transform, glm::vec3(1.0f));
     phongTexObjects.push_back(box);
+    RenderObjectPtr box2 = createTexCube("assets/box.png", 1.0f);
+    box2->transform = glm::translate(box2->transform, glm::vec3(1.0f, 0.3501f, 3.0f));
+    box2->transform = glm::scale(box2->transform, glm::vec3(0.7f));
+    phongTexObjects.push_back(box2);
 
     // Phong colored objects
     RenderObjectPtr wall = createClrCube(glm::vec3(1.0f, 0.5f, 0.0f));
@@ -261,12 +278,14 @@ int main()
 
     // Depth map quad object
     RenderObjectPtr depthQuad = createTexQuad();
-    depthQuad->textureId = depthMap;
 
     // light cube
     coloredObjects.push_back(createLightCube(pointLight->position));
 
     PerformanceMonitor pMonitor(glfwGetTime(), 0.5f);
+
+    // Use fcae culling to fix peter panning problwms with shadows
+    //glEnable(GL_CULL_FACE);
 
     // render loop
     // -----------
@@ -325,28 +344,24 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // 1. render depth of scene to texture (from light's perspective)
+        // 1. render depth of scene to texture (for each light type)
         // --------------------------------------------------------------
-        glm::mat4 lightProjection, lightView;
-        glm::mat4 lightSpaceMatrix;
-        float nearPlane = 0.1f, farPlane = 17.5f;
-        float orthoDim = 10.0f;
-        lightProjection = glm::ortho(-orthoDim, orthoDim, -orthoDim, orthoDim, nearPlane, farPlane);
-        glm::vec3 dirLightPos = glm::vec3(-4.0f, 4.0f, 0.0f);
-        lightView = glm::lookAt(dirLightPos, dirLightPos + glm::normalize(dirLight->direction), glm::vec3(0.0, 1.0, 0.0));
-        lightSpaceMatrix = lightProjection * lightView;
+        //glCullFace(GL_FRONT);
+
+        // RENDER DEPTH MAP FOR DIRECTIONAL LIGHT
+        dirLight->position = dirLight->direction * -8.0f;
+        dirLight->view = glm::lookAt(dirLight->position, dirLight->position + glm::normalize(dirLight->direction), glm::vec3(0.0, 1.0, 0.0));
+        dirLight->spaceMatrix = dirLight->projection * dirLight->view;
         // render the scene to the buffer
         depthMappingShader.use();
-        depthMappingShader.setMat4("lightSpaceMat", lightSpaceMatrix);
+        depthMappingShader.setMat4("lightSpaceMat", dirLight->spaceMatrix);
         glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
-        glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+        glBindFramebuffer(GL_FRAMEBUFFER, (dirLight->depthMapFBO));
         glClear(GL_DEPTH_BUFFER_BIT);
-        //glActiveTexture(GL_TEXTURE0);
         // Render the textured objects
         for(auto& toRender: phongTexObjects) {
             depthMappingShader.setMat4("model", toRender->transform);
             // bind textures on corresponding texture units
-            //glBindTexture(GL_TEXTURE_2D, toRender->textureId);
             glBindVertexArray(toRender->VAO);
             glDrawElements(GL_TRIANGLES, toRender->indexCount, GL_UNSIGNED_INT, 0);
         }
@@ -364,6 +379,7 @@ int main()
         glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        //glCullFace(GL_BACK);
         // 2. render scene as normal using the generated depth/shadow map  
         // --------------------------------------------------------------
 
@@ -381,11 +397,13 @@ int main()
         else if (currentLighting==ELightType::Directional)
         {
             currentLightTexShader->setVec3("light.direction", dirLight->direction);
-            currentLightTexShader->setVec3("light.position", dirLightPos);
-            currentLightTexShader->setMat4("lightSpaceMat", lightSpaceMatrix);
+            currentLightTexShader->setVec3("light.position",  dirLight->position);
+            currentLightTexShader->setMat4("lightSpaceMat", dirLight->spaceMatrix);
             currentLightTexShader->setVec3("light.ambient", dirLight->ambient);
             currentLightTexShader->setVec3("light.diffuse", dirLight->diffuse);
             currentLightTexShader->setVec3("light.specular", dirLight->specular);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, dirLight->depthMap);
         }
         else if (currentLighting==ELightType::Spot)
         {
@@ -417,8 +435,6 @@ int main()
             // bind textures on corresponding texture units
             glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_2D, toRender->textureId);
-            glActiveTexture(GL_TEXTURE1);
-            glBindTexture(GL_TEXTURE_2D, depthMap);
             glBindVertexArray(toRender->VAO);
             glDrawElements(GL_TRIANGLES, toRender->indexCount, GL_UNSIGNED_INT, 0);
         }
@@ -438,11 +454,13 @@ int main()
         else if (currentLighting==ELightType::Directional)
         {
             currentLightClrShader->setVec3("light.direction", dirLight->direction);
-            currentLightClrShader->setVec3("light.position", dirLightPos);
-            currentLightClrShader->setMat4("lightSpaceMat", lightSpaceMatrix);
+            currentLightClrShader->setVec3("light.position", dirLight->position);
+            currentLightClrShader->setMat4("lightSpaceMat", dirLight->spaceMatrix);
             currentLightClrShader->setVec3("light.ambient", dirLight->ambient);
             currentLightClrShader->setVec3("light.diffuse", dirLight->diffuse);
             currentLightClrShader->setVec3("light.specular", dirLight->specular);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, dirLight->depthMap);
         }
         else if (currentLighting==ELightType::Spot)
         {
@@ -470,8 +488,6 @@ int main()
             currentLightClrShader->setVec3("color", toRender->color);
             currentLightClrShader->setMat4("model", toRender->transform);
             
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, depthMap);
             // bind textures on corresponding texture units
             glBindVertexArray(toRender->VAO);
             glDrawElements(GL_TRIANGLES, toRender->indexCount, GL_UNSIGNED_INT, 0);
@@ -492,10 +508,10 @@ int main()
         if (currentDepthMap != EDepthMap::None) {
             depthDebugShader.use();
             depthDebugShader.setBool("orthographic", true);
-            depthDebugShader.setFloat("nearPlane", nearPlane);
-            depthDebugShader.setFloat("farPlane", farPlane);
+            depthDebugShader.setFloat("nearPlane", dirLight->nearPlane);
+            depthDebugShader.setFloat("farPlane", dirLight->farPlane);
             glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, depthMap);
+            glBindTexture(GL_TEXTURE_2D, dirLight->depthMap);
             glBindVertexArray(depthQuad->VAO);
             glDrawElements(GL_TRIANGLES, depthQuad->indexCount, GL_UNSIGNED_INT, 0);
         }
@@ -634,12 +650,12 @@ RenderObjectPtr createTexCube(string path, float texScale, glm::vec3 ka, glm::ve
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f
     };
     unsigned int indices[] = {
-        0, 1, 2,     2, 3, 0,
+        0, 2, 1,     2, 0, 3,
         4, 5, 6,     6, 7, 4,
         8, 9, 10,    10, 11, 8,
-        12, 13, 14,  14, 15, 12,
+        12, 14, 13,  14, 12, 15,
         16, 17, 18,  18, 19, 16,
-        20, 21, 22,  22, 23, 20        
+        20, 22, 21,  22, 20, 23        
     };
     glGenVertexArrays(1, &cubeObject->VAO);
     glGenBuffers(1, &cubeObject->VBO);
@@ -729,12 +745,12 @@ RenderObjectPtr createClrCube(glm::vec3 color, glm::vec3 ka, glm::vec3 kd, glm::
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
     };
     unsigned int indices[] = {
-        0, 1, 2,     2, 3, 0,
+        0, 2, 1,     2, 0, 3,
         4, 5, 6,     6, 7, 4,
         8, 9, 10,    10, 11, 8,
-        12, 13, 14,  14, 15, 12,
+        12, 14, 13,  14, 12, 15,
         16, 17, 18,  18, 19, 16,
-        20, 21, 22,  22, 23, 20        
+        20, 22, 21,  22, 20, 23            
     };
     glGenVertexArrays(1, &cubeObject->VAO);
     glGenBuffers(1, &cubeObject->VBO);
@@ -769,11 +785,11 @@ RenderObjectPtr createLightCube(glm::vec3 pos)
     RenderObjectPtr lightObject = make_shared<RenderObject>();;
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
          0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
          0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 
         -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
          0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
@@ -790,11 +806,11 @@ RenderObjectPtr createLightCube(glm::vec3 pos)
         -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
 
          0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
          0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
          0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
          0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
 
         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
          0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
@@ -804,11 +820,11 @@ RenderObjectPtr createLightCube(glm::vec3 pos)
         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
 
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
          0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
          0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f
     };
     // first, configure the cube's VAO (and VBO)
     glGenVertexArrays(1, &lightObject->VAO);
